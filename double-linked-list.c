5541c9272c44c5a0596163b6c84f7188946c78ff
LikeDuh <fiskovic@gmail.com>
2014-02-17 09:16:56 +0100

    Update double-linked-list.c

--- double-linked-list.c
+++ double-linked-list.c
@@ -1,49 +1,62 @@
+/* 
+ * File:   main.c
+ * Author: superman
+ *
+ * Created on 8. februar 2014, 21:41
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include "doublelink.h"
 
-typedef int bool;
-typedef void *item;
-//Single linked-list
-typedef struct node_{
-  item *thing;
-  struct node_ *ptr;
-}node;
 
-//Double linked-list, which contains two pointers, the head and the tail.
-typedef struct dlist_{
-  node *head;
-  node *tail;
-}dlist;
 
-void insert(dlist *this, item *thing, bool atTail);
 
-int main(){
-  dlist *dlst = (dlist *) malloc(sizeof(dlist *));
-  dlist lst = {NULL,NULL};
-  
-  node *h1;
-  node *t1;
-  dlist lst2 = {h1, t1};
-  *dlst = lst;
-  *(dlst+1) = lst2; 
- 
-  item thing = (int *) 1453;
- // insert(dlst, &thing, 1);
-  
-  return 0;
+//Main function
+
+int main(int argc, char** argv) {
+
+
+    return (0);
 }
 
-void insert(dlist *this, item *thing, bool atTail){
-  node* new_node = (node*) malloc(sizeof(node*));
-  if(atTail == 1){
-   (*this).head->ptr = new_node;
-    (*(*this).head).thing = thing;
-  }
-  else{
-    (*this).tail = new_node;
-    (*(*this).tail).thing = thing;
-  }
-  free(new_node);
-  
-  
+//XOR two pointers(The TA's suggestion)
+
+node* XOR(void* p1, void* p2) {
+    
+    (void*) (((uintptr_t) p1) ^ ((uintptr_t) p2));
+
+}
+
+/* The insert operation
+ * 
+ */
+void insert(dlist *this, item* thing, bool atTail) {
+
+    node* new_node = (node*) malloc(sizeof (node*));
+
+    new_node->thing = thing;
+    new_node->ptr = (*this->head, NULL);
+
+    XOR(this->head, NULL);  //previous node
+    XOR(NULL, this->tail);  //next node
+    
+    //insert node in the end
+    if (this != NULL && atTail == 1) {
+
+        this->head = XOR(NULL, new_node);
+        
+
+    }
+    
+    //insert node in the front
+    if (this != NULL && atTail == 0) {
+
+        this->tail = XOR(new_node, NULL);
+        
+     }
+    
+
+    free(new_node);
 }
